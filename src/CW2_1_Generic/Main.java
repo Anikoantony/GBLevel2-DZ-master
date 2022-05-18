4fd283571cde83a6d36d0e38ec3bcbcb358a0bc2
Antonio <anikoanton@gmail.com>
2022-05-18 11:00:31 +0300

    12333333

---
+++ src/CW2_1_Generic/Main.java
@@ -0,0 +1,15 @@
+package CW2_1_Generic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gen<String> strGen = new Gen<>("A");
+        System.out.println(strGen.getObj());
+        System.out.println(strGen.getObj());
+        System.out.println(strGen.getObj());
+        System.out.println(strGen.getObj());
+        Gen2<String,Integer> gen2 = new Gen2<>("a",1);
+        System.out.println(gen2.getObj1()+gen2.getObj2());
+        System.out.println(gen2.getObj1()+gen2.getObj2());
+    }
+}
